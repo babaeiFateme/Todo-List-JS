d361b2a064c857c674629e62884f0ae854bb26a9
fatemeh <babaei.f1996@gmail.com>
2024-01-19 21:02:34 +0330

    feat: add edit todo logic

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,9 +6,9 @@ const compeleteBtn = document.querySelector(".compelete-btn");
 const addBtn = document.querySelector(".add-btn");
 const closeBtn = document.querySelector(".close-btn");
 const addModal = document.querySelector(".add-modal");
+const editModal = document.querySelector(".edit-modal");
 const overlay = document.querySelector(".overlay");
 let todos = [];
-
 
 const storedTodos = JSON.parse(localStorage.getItem("todos"));
 if (storedTodos) {
@@ -20,6 +20,7 @@ addBtn.addEventListener("click", () => {
     addModal.classList.remove("hidden");
     overlay.classList.remove("hidden");
 });
+
 closeBtn.addEventListener("click", () => {
     addModal.classList.add("hidden");
     overlay.classList.add("hidden");
@@ -56,11 +57,9 @@ function addList(todos) {
             todo.id
         })"><i class="fas fa-trash fs-18"></i>
             </button>
-            <button class="btn">
+            <button class="btn" onclick="editFunc(${todo.id})">
             <i class="fas fa-edit fs-18"></i>
             </button>
-
-            
             </div>
         </li>
             `;
@@ -101,3 +100,34 @@ function funcComplete(id) {
     addList(todos);
     saveToLocalStorage();
 }
+
+
+const editTodoInput = document.querySelector(".edit-todo-input");
+const saveEditBtn = document.querySelector(".save-edit-btn");
+
+const openEditModal = (id) => {
+    const todoToEdit = todos.find((todo) => todo.id === id);
+    if (todoToEdit) {
+        editModal.classList.remove("hidden");
+        overlay.classList.remove("hidden");
+        editTodoInput.value = todoToEdit.title;
+
+        saveEditBtn.addEventListener("click", () => {
+            todoToEdit.title = editTodoInput.value;
+            editModal.classList.add("hidden");
+            overlay.classList.add("hidden");
+
+            addList(todos);
+            saveToLocalStorage();
+        });
+    }
+};
+
+function editFunc(id) {
+    openEditModal(id);
+}
+
+overlay.addEventListener("click", () => {
+    editModal.classList.add("hidden");
+    overlay.classList.add("hidden");
+});
